adfe968c836adfc709b2e3eabeef079404115f51
Yi Ding <fredericyiding@gmail.com>
2014-11-24 09:24:15 -0500

    Add size-checking of the variable

--- get_address.cpp
+++ get_address.cpp
@@ -6,5 +6,6 @@ int main()
 	int c=7;
 	cout<<c<<endl;
 	cout<<&c<<endl;
-	cout<<sizeof(&c)<<endl;
+	cout<<sizeof(&c)<<endl; // this line is to check size of the address
+	cout<<sizeof(c)<<endl;/* this line is to check size of variable itself*/
 }
